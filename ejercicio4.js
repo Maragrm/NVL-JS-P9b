6057305647da86a0b55b7011693bb4fd7015554b
Maragrm <60449910+Maragrm@users.noreply.github.com>
2020-03-02 00:15:43 +0100

    Add files via upload

---
+++ ejercicio4.js
@@ -0,0 +1,19 @@
+var a = "Lunes, Martes, Miercoles, Jueves, Viernes, Sabado, Domingo";
+var b = "Madrid.Sevilla.Barcelona.Valencia.Zaragoza.Bilbao.Donosti";
+var c = a.split(", ");
+var d = b.split(".");
+var e = [c,d];
+console.log(c);
+console.log(e[0][1]);
+
+
+
+
+
+
+ 
+
+
+
+
+
